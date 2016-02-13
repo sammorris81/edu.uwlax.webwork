2e9d21dcbce748841ba9b3f5f52c97420f7a3bba
Sam Morris <sam.morris81@gmail.com>
2016-02-12 22:44:38 -0600

    starting a new macro file for algebra generation macros

---
+++ macros/uwlAlgMacros.pl
@@ -0,0 +1,12 @@
+sub _uwlAlgMacros_init {};
+
+# sample in range - by default call perl if length = 1 otherwise R
+# use argument names in subroutines
+# min => 10, max => 15, replace => TRUE,
+
+sub getArray = {
+
+}
+
+
+1; #required at end of file - a perl thing
