250727d06e9872b89f405df84055100685f56f00
Sam Morris <sam.morris81@gmail.com>
2016-02-13 11:56:22 -0600

    getting the random_variable macro to add variables into the context

--- macros/uwlAlgMacros.pl
+++ macros/uwlAlgMacros.pl
@@ -16,8 +16,29 @@ sub random_variable {
     my $howmany = shift // 1;
     my $letter;
 
-    ($letter) = main::rserve_eval('sample(x = letters, size = ' . $howmany . ', replace = FALSE)');
-    return($letter);
+    @letters = main::rserve_eval('sample(x = letters, size = ' . $howmany . ', replace = FALSE)');
+
+    foreach my $letter (@letters) {
+      main::Context()->variables->add($letter => 'Real');
+    }
+
+    if ($howmany == 1) {
+      return($letters[0]);
+    } else {
+      return @letters;
+    }
+}
+
+sub random_number {
+  my $howmany = shift // 1;
+  my %options = @_;
+
+  my $min = defined($options{'min'}) ? $options{'min'} : -5;
+  my $max = defined($options{'max'}) ? $options{'max'} : 5;
+  my $type = defined($options{'type'}) ? $options{'type'} : "integer";
+
+
+
 }
 
 1; #required at end of file - a perl thing
