08821ea66f2b29aed5d5b5ee4adb5426826d0dc6
Robert F. Allen <rallen@uwlax.edu>
2016-02-13 12:35:27 -0600

    added add_variable_to_Context procedure

--- macros/uwlAlgMacros.pl
+++ macros/uwlAlgMacros.pl
@@ -7,20 +7,50 @@ sub _uwlAlgMacros_init {};
 package uwlAlgMacros;
 
 @ISA = qw(Exporter);
-@EXPORT_OK = qw(random_variable);
+@EXPORT_OK = qw(random_variable, add_variable_to_Context);
 
+##
+ # Consistant formatting of MathObjects
+ ##
+main::Context()->noreduce('(-x)+y','(-x)-y');  # Formula will keep initial negatives
+
+##
+ # Macros for writing Solution section
+ ##
 $clt = "Combine like terms";
-$solcol = "red";
+$solcol = "OrangeRed";
 
+##
+ # Adds variables to the main::Context, skipping x.  
+ # 
+ # @param  variables    list of variables to add 
+ # @param  type         type of variables (default = 'Real')
+ ##
+sub add_variable_to_Context {
+    my %options = @_;
+    my $varlist;
+
+    @varlist = $options{'variables'};
+    my $type = defined($options{'type'}) ? $options{'type'} : 'Real';
+
+    foreach my $var ($varlist) {
+      if $var != 'x' {
+        main::Context()->variables->add($var => $type);
+      }
+    }
+}
+
+##
+ # Returns a list of random letters from a .. z.  
+ # 
+ # @param  size   the number of variables to generate (default = 1).
+ # @return        a list if size > 1 and a single variable if size = 1.       
+ ## 
 sub random_variable {
     my $howmany = shift // 1;
     my $letter;
 
     @letters = main::rserve_eval('sample(x = letters, size = ' . $howmany . ', replace = FALSE)');
-
-    foreach my $letter (@letters) {
-      main::Context()->variables->add($letter => 'Real');
-    }
 
     if ($howmany == 1) {
       return($letters[0]);
