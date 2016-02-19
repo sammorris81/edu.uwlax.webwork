d388626b1a1ad36a5e06bdff2747253eb7ab4e8e
Robert F. Allen <rallen@uwlax.edu>
2016-02-19 11:18:52 -0600

    added subroutine to format solution output for adding/subtracting terms

--- macros/uwlAlgMacros.pl
+++ macros/uwlAlgMacros.pl
@@ -8,18 +8,21 @@ package uwlAlgMacros;
 
 @ISA = qw(Exporter);
 
-@EXPORT_OK = qw(random_variable, random_number, add_variable_to_Context);
-
-##
- # Consistant formatting of MathObjects
- ##
-main::Context()->noreduce('(-x)+y','(-x)-y');  # Formula will keep initial negatives
+@EXPORT_OK = qw(random_variable, random_number, add_variable_to_Context, initialize_Context,solution_addsub_move_format); 
 
 ##
  # Macros for writing Solution section
  ##
-$clt = "Combine like terms";
-$solcol = "OrangeRed";
+our $clt = "Combine like terms";
+our $solcol = "OrangeRed";
+
+##
+ # Consistant formatting of MathObjects
+ ##
+sub initialize_Context {
+  main::Context()->texStrings();
+  main::Context()->noreduce('(-x)+y','(-x)-y');  # Formula will keep initial negatives  
+}
 
 ##
  # Adds variables to the main::Context, skipping x.
@@ -47,20 +50,19 @@ sub add_variable_to_Context {
  # @return        a list if size > 1 and a single variable if size = 1.
  ##
 sub random_variable {
-    my $howmany = shift // 1;
-    my $letter;
+  my $howmany = shift // 1;
+  my $letter;
 
-    @letters = main::rserve_eval('sample(x = letters, size = ' . $howmany . ', replace = FALSE)');
+  @letters = main::rserve_eval('sample(x = letters, size = ' . $howmany . ', replace = FALSE)');
 
-    if ($howmany == 1) {
-      return($letters[0]);
-    } else {
-      return @letters;
-    }
+  if ($howmany == 1) {
+    return($letters[0]);
+  } else {
+    return @letters;
+  }
 }
 
 sub random_number {
-
   my %options = @_;
   my $n = defined($options{'n'}) ? $options{'n'} : 1;
   my $min = defined($options{'min'}) ? $options{'min'} : -5;
@@ -77,6 +79,10 @@ sub random_number {
   if ($zero eq "FALSE") {
     main::rserve_eval('possible <- possible[possible != 0]');
   }
+  if (defined($options{'exclude'})) {
+    my $exclude = $options{'exclude'};
+    main::rserve_eval('possible <- possible[abs(possible) != ' . $exclude . ']');
+  }
   @numbers = main::rserve_eval('sample(x = possible, size = ' . $n . ', replace = ' . $replace . ')');
 
   if ($n == 1) {
@@ -85,6 +91,41 @@ sub random_number {
     return @numbers;
   }
 
+}
+
+sub solution_addsub_move_format {
+  my %options = @_;
+  my $value = $options{'value'};
+  my $includeVariable = "FALSE";
+  my $var; my $move; my $invop;
+
+  if (defined($options{'variable'})) {
+    $includeVariable = "TRUE";
+    $var = $options{'variable'};
+  }
+
+  if ($value < 0) {
+    $action = "Add ";
+    $symbol = " + ";
+    $text = abs($value);
+  } else {
+    $action = "Subtract ";
+    $symbol = " - ";
+    $text = $value;
+  }
+
+  if ($includeVariable eq "TRUE") {
+    if ($value == 1) {
+      $text = $var;
+    } else {
+      $text = $text . $var;
+    }
+  }
+ 
+  $move = $action . $text;
+  $invop = $symbol . $text;
+
+  return ($move, $invop);
 }
 
 1; #required at end of file - a perl thing
