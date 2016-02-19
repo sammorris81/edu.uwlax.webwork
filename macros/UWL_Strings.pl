56c5964fcb5ce25c1d4e06ad9b92cd029d360a18
Sam Morris <sam.morris81@gmail.com>
2016-02-19 16:16:06 -0600

    strings for usage in questions

---
+++ macros/UWL_Strings.pl
@@ -0,0 +1,9 @@
+$INTERVAL_MESSAGE = 'Note: Use INF for \( \infty \), -INF for \( -\infty \), and U for the union symbol, if necessary.';
+$NO_SOLUTION_MESSAGE = 'Note: If there is no solution, enter NONE.';
+$LIMIT_MESSAGE = 'Note: Use INF for \(\infty\), -INF for \(-\infty\), and DNE if the limit does not exist.';
+$INFINITY_MESSAGE = 'Note:  Use INF for \(\infty\) and -INF for \(-\infty\), if necessary.';
+$INTEGRATION_CONSTANT_MESSAGE = 'Note: Use "C" for any arbitrary constant of integration in your answer.';
+$LINE_MESSAGE = 'Note: Provide your answer in slope-intercept form \( y = mx + b \).';
+$LIST_MESSAGE = 'Note: Provide your answer as a comma-separated list if there exists more than one answer.';
+$POINT_MESSAGE = 'Note: Provide your answer as a coordinate point (x,y).';
+$RADIAN_MESSAGE = 'Note: Provide your answer for angles in radians.';
